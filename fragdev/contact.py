7b78e3eb3c2165b3c65d02c8c4afdef11accecaa
Windigo <jacob@fragdev.com>
2013-10-26 14:22:18 -0700

    Contact Form Improvements - Added anti-spam validation field - Improved form styles 	+ Made text fields 1/2 width (min. of 10em) 	+ Made submit button transition when active/hovered 	+ Added focus/hover color on fields to illustrate active field - Removed CSS3 double-colon on pseudo elements to improve IE support

--- fragdev/contact.py
+++ fragdev/contact.py
@@ -1,6 +1,14 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
+# Anti-spam validation
+def validate_ham(value):
+	if value != 'power':
+		raise ValidationError('Anti-spam value incorrect: you may be a robot')
+
+# The form for the Contact page
 class ContactForm(forms.Form):
 	name = forms.CharField(max_length=100)
 	email = forms.EmailField()
 	message	= forms.CharField(widget=forms.Textarea)
+	verify = forms.CharField(label='Anti-spam: Type in the word "power"',validators=[validate_ham],max_length=5)
