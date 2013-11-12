c6550a0990a7ae54420cb78e78b943997cd127c1
Windigo <jacob@fragdev.com>
2013-11-12 15:39:17 -0800

    Added Tag Cloud URL Mapping

--- wiblog/urls.py
+++ wiblog/urls.py
@@ -4,11 +4,10 @@ from wiblog.feeds import PostFeedAtom, PostFeedRSS
 
 urlpatterns = patterns('wiblog.views',
 	url(r'^archive/?$', 'archive', name='archive'),
-	#url(r'^archive/(?P<year>\d{4})/$', 'archive', name='year'),
 	url(r'^feeds/atom/?$', PostFeedAtom(), name='rss'),
 	url(r'^feeds/rss/?$', PostFeedRSS(), name='atom'),
+	url(r'^tagged/(?P<tag>.+)/$', 'tagged_posts', name='tagged'),
 	url(r'^tags/?$', 'tags', name='tags'),
-	url(r'^tags/(?P<tag>.+)/$', 'tags', name='tag'),
 	url(r'^(?P<slug>.+)$', 'post', name='post'),
 	url(r'^$', 'index', name='index'),
 )
