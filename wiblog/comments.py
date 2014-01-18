28e35dddc190b76195b8ae31f0b50903398601bd
Windigo <jacob@fragdev.com>
2014-01-18 11:04:00 -0800

    Work On The Comment System (Anti-Spam, Add HTML + CSS)

--- wiblog/comments.py
+++ wiblog/comments.py
@@ -1,12 +1,10 @@
+from fragdev.contact import validate_ham
 from django.forms import ModelForm
+from django import forms
 from wiblog.models import Comment
 
 class CommentForm(ModelForm):
+	verify = forms.CharField(label='Anti-spam: Type in the word "power"',validators=[validate_ham],max_length=5)
 	class Meta:
 		model = Comment
 		fields = ('name', 'url', 'comment')
-
-#	def __init__(self, *args, **kwargs):
-#			user = kwargs.pop('post','')
-#			super(DocumentForm, self).__init__(*args, **kwargs)
-#			self.fields['user_defined_code']=forms.ModelChoiceField(queryset=UserDefinedCode.objects.filter(owner=user))
