4157c23fa0d79192c75c1e90d98a4e84443211b2
Windigo <jacob@fragdev.com>
2014-02-22 13:44:54 -0800

    Fixed Order Of Feeds To Match URLs

--- wiblog/urls.py
+++ wiblog/urls.py
@@ -4,8 +4,8 @@ from wiblog.feeds import PostFeedAtom, PostFeedRSS
 
 urlpatterns = patterns('wiblog.views',
 	url(r'^archive/?$', 'archive', name='archive'),
-	url(r'^feeds/atom/?$', PostFeedAtom(), name='rss'),
-	url(r'^feeds/rss/?$', PostFeedRSS(), name='atom'),
+	url(r'^feeds/atom/?$', PostFeedAtom(), name='atom'),
+	url(r'^feeds/rss/?$', PostFeedRSS(), name='rss'),
 	url(r'^tagged/(?P<tag>.+)/$', 'tagged_posts', name='tagged'),
 	url(r'^tags/?$', 'tags', name='tags'),
 	url(r'^(?P<slug>.+)$', 'post', name='post'),
