b3c4dd551f59f30a3c1db715b9c25d01abbe1b23
Windigo <jacob@fragdev.com>
2016-08-13 23:58:35 -0700

    Add basic views to the project app

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from models import project
 
-# Create your views here.
+def index(request):
+    '''
+    Project listing
+    '''
+
+    projects = Project.objects.filter(status=Project.PUBLIC).order_by('-date')
+
+
+    return HttpResponse(template.render({'projects': projects}))
+
+
+def project(request, slug):
+    '''
+    Display project details
+    '''
+
+    try:
+        project = Project.objects.get(slug=slug)
+    except ObjectDoesNotExist:
+
+        #TODO: Maybe a better option, like 404?
+        pass
+        #return redirect('')
+
+    return HttpResponse(template.render({'projects': projects}))
