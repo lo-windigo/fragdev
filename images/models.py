3a7476116963c0f7f173bfbead49d2340f2c2321
Windigo <jacob@fragdev.com>
2016-08-23 15:48:47 -0700

    Minimum viable app

--- images/models.py
+++ images/models.py
@@ -1,19 +1,52 @@
+# This file is part of FragDev.
+#
+# FragDev is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# FragDev is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with FragDev.  If not, see <http://www.gnu.org/licenses/>.
+
 from django.db import models
+from django.core.urlresolvers import reverse
 from django.utils.text import slugify
+from imghdr import what
 
 
 class Image(models.Model):
     '''
     Represents a single uploaded image
     '''
-    desc = models.CharField(max_length=250)
+    title = models.CharField(max_length=250)
+    desc = models.TextField()
     date = models.DateTimeFiel(auto_now_add=True)
     imgFile = models.FileField(upload_to='img/')
-    slug = models.SlugField(blank=True)
+    slug = models.SlugField()
+    content_type = models.CharField(max_length=30)
 
     def save(self, *args, **kwargs):
 
+        # Create a slug for this image
         if not self.id and self.slug is None:
-            self.slug = slugify(self.desc)
+            self.slug = slugify(self.title)
+
+        # Generate different versions
+        # TODO
+
+        # Save the content type (required for headers later)
+        self.content_type = what(imgFile)
 
         super(Image, self).save(*args, *kwargs)
+
+
+    def get_absolute_url(self):
+        return reverse("images:index", args=[self.slug])
+
+    def __str__(self):
+        return self.title
--- images/views.py
+++ images/views.py
@@ -1,15 +1,33 @@
+# This file is part of FragDev.
+#
+# FragDev is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# FragDev is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with FragDev.  If not, see <http://www.gnu.org/licenses/>.
+
 from django.http import Http404, Http500, HttpResponse
 
-# Create your views here.
+
 def index(request, slug):
+    '''
+    Return a single image
+    '''
 
     # Try to get the requested post
     try:
         image = Image.objects.get(slug=slug)
 
-        while open(image.open("rb")) as imageFile:
+        while open(image.imgFile.open("rb")) as imageFile:
             return HttpReponse(imageFile.read(),
-                content_type=image.content_type)
+                content_type="image/{}".format(image.content_type))
 
     except ObjectDoesNotExist:
         raise Http404("Image does not exist")
