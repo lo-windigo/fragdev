32361ae71b3f48f6c238b9327b69cae2f33a261a
Windigo <jacob@fragdev.com>
2016-10-25 15:45:59 -0700

    Add license statement, and register Project model with the admin site

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin
+# This file is part of the FragDev Website.
+# 
+# the FragDev Website is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# the FragDev Website is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with the FragDev Website.  If not, see <http://www.gnu.org/licenses/>.
 
-# Register your models here.
+from django.contrib import admin
+from .models import Project
+
+admin.site.register(Project)
+
