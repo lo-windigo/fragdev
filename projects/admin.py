29df62705780302b698db7341ddef9bcec6a2292
Windigo <jacob@fragdev.com>
2016-10-26 13:02:39 -0700

    Automatically populate slug field in admin

--- projects/admin.py
+++ projects/admin.py
@@ -16,5 +16,10 @@
 from django.contrib import admin
 from .models import Project
 
-admin.site.register(Project)
+
+class ProjectAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+
+
+admin.site.register(Project, ProjectAdmin)
 
