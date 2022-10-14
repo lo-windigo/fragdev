5593c5483ca60118751ccfb36080ef430ba21cd0
Windigo <jacob@fragdev.com>
2022-10-13 20:30:03 -0700

    Define DEFAULT_AUTO_FIELD to prevent warnings

--- fragdev/settings.py
+++ fragdev/settings.py
@@ -43,6 +43,9 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+# Specify what field type is used for automatic primary keys
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+
 ##################
 # Local settings #
 ##################
